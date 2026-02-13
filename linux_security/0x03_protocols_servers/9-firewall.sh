11262aab9abddc120cf5bc246a1a062ea04ba6c1
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2026-02-13 10:23:55 -0500

    task 9 added

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 iptables -A INPUT -p tcp --dport ssh -j ACCEPT
-iptables -A INPUT -m state --state ESTABL, -j C  iptables -  -j P
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT && iptables -A INPUT -j DROP
