9ac90460e29261afe72266502f67ef71976d6f20
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2026-02-13 10:19:09 -0500

    task 6 added

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "^smtpd_tls_security_level *= *(may|encrypt)" /etc/postfix/main.cf || echo "STARTTL not configured
+grep -E "^smtpd_tls_security_level *= *(may|encrypt)" /etc/postfix/main.cf || echo "STARTTLS not configured"
