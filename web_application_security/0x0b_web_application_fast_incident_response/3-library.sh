366b4e6c9369b2b71bf2dac27ba53c07e041cc57
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2026-04-10 11:11:47 -0400

    task 3

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat logs.txt | awk '{print $12}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+cat logs.txt | awk -F '"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
