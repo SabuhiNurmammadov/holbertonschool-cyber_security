badd6e42ebd9145467c3e68152b52a223ac0c8e6
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2026-04-10 10:57:01 -0400

    task 0

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,1 +1,1 @@
-cat logs.txt | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+grep -v '^#' logs.txt | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
